a0b4491304e94a51b99544ae53f4ff003951c299
SeongWonSeol <132084868+SeongWonSeol@users.noreply.github.com>
2023-12-06 21:05:12 +0900

    2023-12-16
    complete swordSplash and animation

--- Lab/Assets/Script/SwordSplashAnimationManage.cs
+++ Lab/Assets/Script/SwordSplashAnimationManage.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEditor;
 using UnityEngine;
 
 public class SwordSplashAnimationManage : MonoBehaviour
 {
     private Animator animator;
     private bool playedAnimation = false;
-    private int playAnimationIndex = 0;
+    private int playAnimationIndex = 1;
+
+    public int delayTime = 800;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +23,39 @@ public class SwordSplashAnimationManage : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) && !playedAnimation)
         {
-            playedAnimation = true;
-            animator.Play("SwordSplash1");
-            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f)
-            {
-                animator.Play("Notting");
-            }
+            playAnimation();
         }
+    }
+
+    async void playAnimation()
+    {
+        if (playAnimationIndex == 1)
+        {
+            animator.Play("SwordSplash1");
+            playedAnimation = true;
+        }
+
+        if (playAnimationIndex == 2)
+        {
+            animator.Play("SwordSplash2");
+            playedAnimation = true;
+        }
+
+        if (playAnimationIndex == 3)
+        {
+            animator.Play("SwordSplash3");
+            playedAnimation = true;
+        }
+
+        playAnimationIndex++;
+
+        if (playAnimationIndex == 4)
+        {
+            playAnimationIndex = 1;
+        }
+
+        await Task.Delay(delayTime);
+
+        playedAnimation = false;
     }
 }
